aa07076362326a5e847fd0abe8918f1f04cc1f1a
paulabarreto <paulavilaca@gmail.com>
2018-10-10 00:10:32 +0000

    refactored

--- main.rb
+++ main.rb
@@ -1,32 +1,3 @@
-require_relative './question.rb'
-require_relative './player.rb'
-require_relative './turn.rb'
-# require_relative './game.rb'
+require_relative './game.rb'
 
-
-def random_number
-  rand(1...6)
-end
-
-player1 = Player.new(1)
-player2 = Player.new(2)
-turn = Turn.new(player1)
-turn2 = Turn.new(player2)
-
-
-while player1.lives > 0 && player2.lives > 0
-  first_question = Question.new(random_number, random_number, turn.player.name)
-  puts first_question.generate_questions
-  answer = player1.answer_question
-  result = first_question.compare_answers?(answer)
-  puts player1.update_life_count(result)
-  if player1.lives > 0
-    puts turn2.change_turns
-    next_question = Question.new(random_number, random_number, player2.name)
-    puts next_question.generate_questions
-    answer2 = player2.answer_question
-    result2 = next_question.compare_answers?(answer2)
-    puts player2.update_life_count(result2)
-  end
-end
-puts turn.game_over
+Game.new
